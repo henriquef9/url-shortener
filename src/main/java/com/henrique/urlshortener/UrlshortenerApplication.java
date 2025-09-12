036245db3544048ce4370790b7d2fdba0172a9af
Antônio Henrique Pacheco Costa <102102220+henriquef9@users.noreply.github.com>
2025-09-12 19:21:55 -0300

    fix: resolve Swagger UI CORS issue

--- src/main/java/com/henrique/urlshortener/UrlshortenerApplication.java
+++ src/main/java/com/henrique/urlshortener/UrlshortenerApplication.java
@@ -1,11 +1,14 @@
 package com.henrique.urlshortener;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 public class UrlshortenerApplication {
 
 	public static void main(String[] args) {
